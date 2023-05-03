fcd9a8432086db89b5a0e510f5ac7ebc191515cb
Mikael Rönnberg <mikael.ronnberg@medieinstitutet.se>
2023-05-03 16:29:46 +0200

    Feat: Updated Animal.tsx

--- src/pages/Animal/Animal.tsx
+++ src/pages/Animal/Animal.tsx
@@ -1,5 +1,25 @@
 import "./Animal.css";
 
-export const Animal = () => {
-  return <></>;
+export interface IAnimal {
+  id: number;
+  imageUrl: string;
+  isFed: boolean;
+  lastFed: string;
+  latinName: string;
+  longDescription: string;
+  medicine: string;
+  name: string;
+  shortDescription: string;
+  yearOfBirth: number;
+}
+
+export const Animal = (animalProps: IAnimal) => {
+  return (
+    <>
+      <div className="animal-wrapper">
+        <h2>{animalProps.name}</h2>
+        <img src={animalProps.imageUrl} alt={animalProps.name}></img>
+      </div>
+    </>
+  );
 };
